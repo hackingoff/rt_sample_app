8fe558d74a7749dc0bf69589f8aa465491bb2b24
Hacking Off <source@hackingoff.com>
2013-06-09 14:08:39 -0700

    Added terrible non-DRY static pages for home/help/about and added title tests to static_pages_spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,6 +10,11 @@ describe "StaticPages" do
   #end
 
   describe "Home page" do
+  	it "should have the title 'Home'" do
+  		visit '/static_pages/home'
+  		expect(page).to have_title("Home | Ruby on Rails Tutorial Sample App")
+  	end
+
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
@@ -17,10 +22,26 @@ describe "StaticPages" do
   end
 
   describe "Help page" do
+  	it "should have the title 'Help'" do
+  		visit '/static_pages/help'
+  		expect(page).to have_title("Help | Ruby on Rails Tutorial Sample App")
+  	end
 
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+  end
+
+  describe "About page" do
+  	it "should have the title 'About'" do
+  		visit '/static_pages/about'
+  		expect(page).to have_title("About | Ruby on Rails Tutorial Sample App")
+  	end
+
+  	it "should have the content 'About" do
+  		visit '/static_pages/about'
+  		expect(page).to have_content('About')
+  	end
   end
 end
