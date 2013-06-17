6741bfe9e92c5c1b06ad780b1e8c05cb016deed0
Hacking Off <source@hackingoff.com>
2013-06-16 22:21:48 -0700

    Tweaked application.html.erb, and split shim, header, and footer off into partials.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,7 +9,7 @@ describe "StaticPages" do
   #  end
   #end
   let(:title_slug) { "Ruby on Rails Tutorial Sample App" }
-  
+
   describe "Home page" do
   	it "should have the base title" do
   		visit '/static_pages/home'
@@ -19,6 +19,11 @@ describe "StaticPages" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
+    end
+
+    it "should not have a custom title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
