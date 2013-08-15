5c3afe15ee5f1777bfc35e4a265a610935173134
Hacking Off <source@hackingoff.com>
2013-08-14 20:38:53 -0700

    finished first 2 exercises of ch5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,33 +10,44 @@ describe "StaticPages" do
   #end
 
   subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
     
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
+    let(:heading) { 'Sample App'}
+    let(:page_title) {''}
+
+    it_should_behave_like "all static pages"
     it { should_not have_title("| Home") }
   end
 
   describe "Help page" do
     before { visit help_path }
-  	
-    it { should have_title(full_title('Help')) }
-    it { should have_content('Help') }
+
+    let(:heading) { 'Help'}
+    let(:page_title) {'Help'}
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path } # translates to '/static_pages/about'
-  	
-    it { should have_title(full_title("About Us")) }
-    it { should have_content("About Us") }
+
+    let(:heading) { 'About Us'}
+    let(:page_title) {'About Us '}
+    it_should_behave_like "all static pages"
   end
 
   # Exercise 3.1 from http://ruby.railstutorial.org/chapters/static-pages?version=4.0#code-serve_static_assets_redux
   describe "Contact page" do
     before { visit contact_path } # '/static_pages/contact'
-    
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
+
+    let(:heading) { 'Contact'}
+    let(:page_title) {'Contact'}
+    it_should_behave_like "all static pages"
   end
 end
